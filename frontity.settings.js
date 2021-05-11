c09fe326070f9c3a87886a1b89bd8080abda6add
blh286 <marcus.nygren@teliacompany.com>
2021-05-11 20:43:50 +0200

    Frontify init with WordPress as backend

---
+++ frontity.settings.js
@@ -0,0 +1,41 @@
+const settings = {
+  "name": "my-app",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        source: {
+          url: "http://hi.marcusnygren.com"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
